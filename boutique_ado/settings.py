4672e658e4a75571a427722b61a11340f49cde73
Saranya Jayakumaran <jsaran.bj@gmail.com>
2024-09-09 13:52:01 +0000

    Created home page with css

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+k_is5%2%lzt3t0o@+^(f)*8ts)x2i7lcm4_rm68nwajk5r%(_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000','8000-saranyajaya-botiqueadov-rcuovwdlbdr.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['127.0.0.1:8000','8000-saranyajaya-botiqueadov-40doqd1lb3f.ws.codeinstitute-ide.net']
 
 
 # Application definition
@@ -150,6 +150,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
