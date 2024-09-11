09701e0e6112ac773d100345862ed85482aa4546
Saranya Jayakumaran <jsaran.bj@gmail.com>
2024-09-11 14:39:16 +0000

    Add products to home page

--- products/views.py
+++ products/views.py
@@ -2,15 +2,14 @@ from django.shortcuts import render
 from .models import Product
 
 # Create your views here.
+
 def all_products(request):
-    """
-    A view to show all products, including sorting and search queries
-    """
-    products=Product.objects.all()
+    """ A view to show all products, including sorting and search queries """
+
+    products = Product.objects.all()
 
     context = {
-        'products':products,
-
+        'products': products,
     }
 
-    return render(request, 'products/products.html',context)
+    return render(request, 'products/products.html', context)
